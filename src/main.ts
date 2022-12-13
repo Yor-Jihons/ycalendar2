05633cac80b5ac92567574fc52951921299bf5c0
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-13 10:14:36 +0900

    Transplanted the function createClassName from ycalendar.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,28 @@
-function test( n:number ){
-    console.log( "n = " + n );
+import * as Util from "./util";
+
+class Test1{
+    constructor(){
+        
+    }
 }
+
+class Test2{
+    state:boolean;
+    constructor(state:boolean){
+        this.state = state;
+    }
+
+    has( obj:any ){
+        return this.state;
+    }
+}
+
+
+function test( state:boolean ){
+    let test1 = new Test1();
+    let test2 = new Test2( state );
+    let s:string = Util.createClassName( test2, test1 );
+    console.log( "test = " + s );
+}
+
+test(true);
