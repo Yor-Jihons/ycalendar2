ef33c81085ef2038b7e3dea50f14b3ec36717c95
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-17 17:33:34 +0900

    Defined the types.

--- src/main.ts
+++ src/main.ts
@@ -3,35 +3,6 @@ import * as XEvent from "./event";
 import * as CheckedDates from "./checkeddates";
 import * as ycalendars from "./ycalendar";
 
-class Test1{
-    constructor(){
-        
-    }
-}
-
-class Test2{
-    state:boolean;
-    constructor(state:boolean){
-        this.state = state;
-    }
-
-    has( obj:any ){
-        return this.state;
-    }
-}
-
-
-function test( state:boolean ){
-    let test1 = new Test1();
-    let test2 = new Test2( state );
-    let s:string = Util.createClassName( test2, test1 );
-    console.log( "test = " + s );
-
-    let obj1 = new XEvent.Event();
-    obj1.onDoubleClicked( 1, 1, 1 );
-}
-
-test(true);
 
 // ---------------------------------------------------
 // HTMLからも使えるようにする
