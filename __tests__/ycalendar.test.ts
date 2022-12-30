ff51b3b1b32570188a042cc17cac14cbcf014c20
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-30 15:53:05 +0900

    Added the test for ycalendar.ts.

---
+++ __tests__/ycalendar.test.ts
@@ -0,0 +1,8 @@
+import * as CheckedDates from "../src/checkeddates";
+
+
+describe('Test for ycalendar.ts', () => {
+    test( 'Sample of the class ycalendar', () => {
+        //expect( CheckedDates.CheckedDateType.Unchecked ).toBe( 0 );
+    });
+});
