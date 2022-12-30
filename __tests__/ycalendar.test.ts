9ea242130f6a6e57484dcdeda790c60dd012f338
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-30 16:00:29 +0900

    Deleted the test for ycalendar.ts.

--- __tests__/ycalendar.test.ts
+++
@@ -1,8 +0,0 @@
-import * as CheckedDates from "../src/checkeddates";
-
-
-describe('Test for ycalendar.ts', () => {
-    test( 'Sample of the class ycalendar', () => {
-        //expect( CheckedDates.CheckedDateType.Unchecked ).toBe( 0 );
-    });
-});
